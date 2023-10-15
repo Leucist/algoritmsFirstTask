b9d7847c2a3af9f843164bc5bab2a43afc6f8238
leucistic <leucist@yandex.by>
2023-10-15 21:10:26 +0200

    First method completed, some fixes made

--- makeInputFile.py
+++ makeInputFile.py
@@ -6,8 +6,8 @@ intervalsAmount = 1000000
 with open("inputFile.txt", "w") as inputFile:
 	inputFile.write(str(intervalsAmount) + '\n')
 	for i in range(intervalsAmount):
-		a = b = random.randint(1, 1000)
-		while a == b:
-			b = random.randint(1, 1000)
+		a = b = random.randint(1, 999)
+		while a >= b:
+			b = random.randint(2, 1000)
 		inputFile.write(str(a) + ' ' + str(b) + '\n')
 	inputFile.close()
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        File inputFile = new File("in_E_1_Nowik.txt");          /* Initialising file with input intervals */
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("inputFile.txt");          /* Initialising file with input intervals */
         Scanner scanner = new Scanner(inputFile);
 
         int intervalsAmount = scanner.nextInt();
@@ -17,49 +17,47 @@ public class Main {
         }
 
         // sorting
-        int[] intervalsStarts = new int[intervalsAmount];
-        for (int i = 0; i < intervalsAmount; i++) {
-            intervalsStarts[i] = inputIntervals[i][0];              /* Fills the intervalsStarts array */
-        }
-        intervalsStarts = sortArray(intervalsStarts);
-        int[][] buffArray = new int[intervalsAmount][2];
-        for (int i = 0; i < intervalsAmount; i++) {
-
-        }
-
-
-
-        ArrayList<int[]> outputIntervals = new ArrayList<>();
+        inputIntervals = sortArray(inputIntervals);
 
         // adding intervals
-        int[] currentInterval = inputIntervals.get(0);
+        int[][] outputIntervals = new int[intervalsAmount][2];
+        int[] currentInterval = inputIntervals[0];
+        int counter = 0;
+
         for (int i = 1; i < intervalsAmount; i++) {
-            if (currentInterval[1] > inputIntervals.get(i)[0]) {
+            if (currentInterval[1] >= inputIntervals[i][0]) {                /* if two intervals collide */
+                // when arrays are sorted by the beginning of the interval in ascending order =>
+                // ..'intervals[i][0] <= intervals[i+1][0]' is always true.
+
                 // takes the larger end among the two intervals
                 // currentInterval[1] = currentInterval[1] > inputIntervals.get(i)[1] ? currentInterval[1] : inputIntervals.get(i)[1];
-                currentInterval[1] = Math.max(currentInterval[1], inputIntervals.get(i)[1]);
+                currentInterval[1] = Math.max(currentInterval[1], inputIntervals[i][1]);
             }
             else {
-                outputIntervals.add(currentInterval);
-                currentInterval = inputIntervals.get(i);
+                outputIntervals[counter++] = currentInterval;
+                currentInterval = inputIntervals[i];
             }
+        }
+        outputIntervals[counter] = currentInterval;                             /* adds the last interval */
 
-
-
-            // if two intervals collide
-            if (inputIntervals.get(i)[1] > inputIntervals.get(i+1)[0]) {
-                // if arrays are sorted by the beginning of the interval in ascending order =>
-                // ..'intervals[i][0] <= intervals [i+1][0]' is always true.
-
+        // Writes output data into the file
+        try (FileWriter outputFile = new FileWriter("output.txt")) {
+            outputFile.write(Integer.toString(counter+1) + '\n');
+            for (int i = 0; i <= counter; i++) {
+                String output = Integer.toString(outputIntervals[i][0]) + ' ' + Integer.toString(outputIntervals[i][1]) + '\n';
+                outputFile.write(output);
             }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static int[] sortArray(int[] inputArray) {
+    public static int[][] sortArray(int[][] inputArray) {
         if (inputArray == null || inputArray.length < 2) return inputArray;
         // Initialises arrays for dividing the inputArray into two parts
-        int[] childArray1 = new int[inputArray.length / 2];
-        int[] childArray2 = new int[inputArray.length - inputArray.length / 2];
+        int[][] childArray1 = new int[inputArray.length / 2][2];
+        int[][] childArray2 = new int[inputArray.length - inputArray.length / 2][2];
         // Copies values in halves from the inputArray into the child arrays
         System.arraycopy(inputArray, 0, childArray1, 0, childArray1.length);
         System.arraycopy(inputArray, childArray1.length, childArray2, 0, childArray2.length);
@@ -70,8 +68,8 @@ public class Main {
         return mergeArrays(childArray1, childArray2);
     }
 
-    public static int[] mergeArrays(int[] array1, int[] array2) {
-        int[] outputArray = new int[array1.length + array2.length];
+    public static int[][] mergeArrays(int[][] array1, int[][] array2) {
+        int[][] outputArray = new int[array1.length + array2.length][2];
         int pointer1 = 0, pointer2 = 0;
         for (int i = 0; i < outputArray.length; i++) {
             // if one array ends, output is filled with the rest of another
@@ -83,7 +81,7 @@ public class Main {
                 System.arraycopy(array1, pointer1, outputArray, i, outputArray.length - i);
                 break;
             }
-            outputArray[i] = array1[pointer1] < array2[pointer2] ? array1[pointer1++] : array2[pointer2++];
+            outputArray[i] = array1[pointer1][0] < array2[pointer2][0] ? array1[pointer1++] : array2[pointer2++];
         }
         return outputArray;
     }
